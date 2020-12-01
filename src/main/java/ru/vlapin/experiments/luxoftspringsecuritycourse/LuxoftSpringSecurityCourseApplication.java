8c1b165c729baf9e07c88a7ab56cd86dcc6f2805
Vyacheslav-Lapin <Vyacheslav.Lapin@gmail.com>
2020-12-01 11:19:59 +0300

    Initial commit

---
+++ src/main/java/ru/vlapin/experiments/luxoftspringsecuritycourse/LuxoftSpringSecurityCourseApplication.java
@@ -0,0 +1,13 @@
+package ru.vlapin.experiments.luxoftspringsecuritycourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxoftSpringSecurityCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxoftSpringSecurityCourseApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/vlapin/experiments/luxoftspringsecuritycourse/config/PropertyPlaceholder.java
@@ -0,0 +1,21 @@
+package ru.vlapin.experiments.luxoftspringsecuritycourse.config;
+
+import ru.vlapin.experiments.luxoftspringsecuritycourse.model.JavaConfigBasedSetterPropertiesPlaceholderExample;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@ConfigurationPropertiesScan("ru.vlapin.experiments.luxoftspringsecuritycourse")
+public class PropertyPlaceholder {
+
+  @Bean
+  @ConfigurationProperties("my-properties2")
+  JavaConfigBasedSetterPropertiesPlaceholderExample mySetterProperties2() {
+    return new JavaConfigBasedSetterPropertiesPlaceholderExample();
+  }
+}
